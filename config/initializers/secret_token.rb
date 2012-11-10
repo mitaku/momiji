ca95fb14c11f9410b0231273da8929beaefe3bab
mitaku <mitaku1104@gmail.com>
2012-11-10 15:04:51 +0900

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Momiji::Application.config.secret_token = 'e0d2153d27a4d65715034a85374fd0e05d03ce76e525c9df65c496f025c1662d190df4ad72ac91a2c722a4e04169df5f0449818d6a74515800576a559ea02b6f'
