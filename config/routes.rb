1767cb417df70a3854b309c6c75602aa754e311d
mitaku <mitaku1104@gmail.com>
2012-11-10 20:24:42 +0900

    Add DashboardController

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,11 @@
 Momiji::Application.routes.draw do
+
+  devise_for :users
+
+  resources :companies, :path => '/', :only => [] do
+    get '/' => 'dashboard#index'
+  end
+
   # The priority is based upon order of creation:
   # first created -> highest priority.
 
