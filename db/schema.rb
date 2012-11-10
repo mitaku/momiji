55b980fb6c1581dfe88379f9e0f0e77d725f614c
mitaku <mitaku1104@gmail.com>
2012-11-10 19:04:59 +0900

    rails g devise User

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20121110084346) do
+ActiveRecord::Schema.define(:version => 20121110094701) do
 
   create_table "companies", :force => true do |t|
     t.string   "name"
@@ -24,5 +24,26 @@ ActiveRecord::Schema.define(:version => 20121110084346) do
 
   add_index "companies", ["management_id"], :name => "index_companies_on_management_id"
   add_index "companies", ["type"], :name => "index_companies_on_type"
+
+  create_table "users", :force => true do |t|
+    t.string   "name"
+    t.integer  "company_id"
+    t.string   "email",                  :default => "", :null => false
+    t.string   "encrypted_password",     :default => "", :null => false
+    t.string   "reset_password_token"
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
+    t.integer  "sign_in_count",          :default => 0
+    t.datetime "current_sign_in_at"
+    t.datetime "last_sign_in_at"
+    t.string   "current_sign_in_ip"
+    t.string   "last_sign_in_ip"
+    t.datetime "created_at",                             :null => false
+    t.datetime "updated_at",                             :null => false
+  end
+
+  add_index "users", ["company_id"], :name => "index_users_on_company_id"
+  add_index "users", ["email"], :name => "index_users_on_email", :unique => true
+  add_index "users", ["reset_password_token"], :name => "index_users_on_reset_password_token", :unique => true
 
 end
