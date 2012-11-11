2ab98d94f813cc551493d103b1dd59d5c2b8c1a1
mitaku <mitaku1104@gmail.com>
2012-11-11 17:15:10 +0900

    Add Validations

--- app/models/company.rb
+++ app/models/company.rb
@@ -2,4 +2,13 @@ class Company < ActiveRecord::Base
   attr_accessible :name, :code
 
   has_many :users
+
+  scope :where_code_is, lambda { |code| where(:code => code).limit(1) }
+
+  validates :name, :presence => true
+  validates :code, :uniqueness => true, :presence => true
+
+  def to_param
+    code.to_s
+  end
 end
