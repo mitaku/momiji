c0d08ffebf1a61dba74734a31e10378be92b0541
mitaku <mitaku1104@gmail.com>
2012-11-11 17:15:25 +0900

    ApplicationController#target_company, current_company

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,11 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
+
+  def target_company
+    @target_company ||= Company.where_code_is(params[:company_id])
+  end
+
+  def current_company
+    @current_company ||= current_user.company
+  end
 end
