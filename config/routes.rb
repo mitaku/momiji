83201f7e17f8460bc37823ff50460042a059202a
mitaku <mitaku1104@gmail.com>
2012-11-15 19:27:28 +0900

    Add Ability

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,6 @@ Momiji::Application.routes.draw do
 
 
 
-  mount RailsAdmin::Engine => '/admin', :as => 'rails_admin'
 
   root :to => "welcome#index"
 
@@ -70,4 +69,6 @@ Momiji::Application.routes.draw do
   # This is a legacy wild controller route that's not recommended for RESTful applications.
   # Note: This route will make all actions in every controller accessible via GET requests.
   # match ':controller(/:action(/:id))(.:format)'
+
+  mount RailsAdmin::Engine => '/adm', :as => 'rails_admin'
 end
