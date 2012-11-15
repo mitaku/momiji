88adca75c8eabd8fc32774a8dc12cb491f259a56
mitaku <mitaku1104@gmail.com>
2012-11-15 15:46:45 +0900

    Add WelcomeController signed_in_check

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,4 +1,16 @@
 class WelcomeController < ApplicationController
+  before_filter :signed_in_check
   def index
   end
+
+  private
+
+    def signed_in_check
+      if signed_in?
+        redirect_to company_path(current_user.company.code)
+        return false
+      end
+      true
+    end
+
 end
