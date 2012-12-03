1caf977eec8eb2596a429219cb81b219099d31d1
mitaku <mitaku1104@gmail.com>
2012-12-03 17:18:39 +0900

    add relation

--- app/models/category.rb
+++ app/models/category.rb
@@ -1,3 +1,5 @@
 class Category < ActiveRecord::Base
-  attr_accessible :name, :owner_id, :type
+  attr_accessible :name
+
+  has_many :items, :class_name => "CategoryItem", :dependent => :destroy
 end
