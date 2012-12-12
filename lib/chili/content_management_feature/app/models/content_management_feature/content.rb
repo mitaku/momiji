1305de6fda0163f276e022e8f23b2f24fdfd2cf5
mitaku <mitaku1104@gmail.com>
2012-12-12 20:16:55 +0900

    Add Content And Directory relation

--- lib/chili/content_management_feature/app/models/content_management_feature/content.rb
+++ lib/chili/content_management_feature/app/models/content_management_feature/content.rb
@@ -1,6 +1,7 @@
 module ContentManagementFeature
   class Content < ActiveRecord::Base
     belongs_to :company, :class_name => "::Company"
+    belongs_to :directory
     attr_accessible :name, :data
     mount_uploader :data, ContentUploader
   end
--- lib/chili/content_management_feature/app/models/content_management_feature/directory.rb
+++ lib/chili/content_management_feature/app/models/content_management_feature/directory.rb
@@ -3,6 +3,7 @@ module ContentManagementFeature
     has_ancestry
 
     belongs_to :company, :class_name => "::Company"
+    has_many :contents, :dependent => :destroy
 
     attr_accessible :ancestry, :name
   end
