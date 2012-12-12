26844792ac5ab49a3bdaf97c0c8156c482aedf84
Takumi MIURA <mitaku1104@gmail.com>
2012-12-13 01:10:30 +0900

    Add Directory#handouts, build_handout, create_handout

--- vendor/gems/chocoholic/app/models/chocoholic/directory.rb
+++ vendor/gems/chocoholic/app/models/chocoholic/directory.rb
@@ -1,4 +1,19 @@
 module Chocoholic
   class Directory < Content
+
+    def handouts
+      children.where(:type => 'Chocoholic::Handout')
+    end
+
+    def build_handout(attr = {})
+      h = Chocoholic::Handout.new(attr)
+      h.company_id = self.company_id
+      h.parent = self
+      h
+    end
+
+    def create_handout(attr = {})
+      h = build_handout(attr).save
+    end
   end
 end
