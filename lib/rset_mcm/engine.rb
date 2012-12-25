63e4e4f30b58b41c3055411630645ba6f09240cf
Takumi Miura <takumi.miura@optim.co.jp>
2012-12-25 20:54:05 +0900

    Set Application Modules

--- lib/rset_mcm/engine.rb
+++ lib/rset_mcm/engine.rb
@@ -1,5 +1,11 @@
 module RsetMCM
   class Engine < ::Rails::Engine
     isolate_namespace RsetMCM
+    config.generators do |g|
+      g.template_engine :haml
+      g.javascript_engine :coffee
+      g.test_framework :rspec #, view_specs: false, routing_specs: false, controller_specs: false
+      g.integration_tool :rspec
+    end
   end
 end
