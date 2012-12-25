405e69d92e0bdb633dc5300cf71f390d4d21e051
Takumi Miura <takumi.miura@optim.co.jp>
2012-12-26 00:02:44 +0900

    Load HandoutUploader

--- app/models/rset_mcm/handout.rb
+++ app/models/rset_mcm/handout.rb
@@ -3,8 +3,19 @@ module RsetMcm
     attr_accessible :data
     attr_accessible :tag_list
 
+    mount_uploader :data, HandoutUploader
+
+    before_validation :update_data_attributes
+
     def directory?
       false
+    end
+
+    private
+    def update_data_attributes
+      if data.present? && data_changed?
+        self.file_size = data.file.size
+      end
     end
   end
 end
