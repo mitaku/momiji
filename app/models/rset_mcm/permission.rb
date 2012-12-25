85d70b89851ab20971c2dae67636ba6c237aeb96
Takumi Miura <takumi.miura@optim.co.jp>
2012-12-25 22:57:02 +0900

    Write Permission Spec

--- app/models/rset_mcm/permission.rb
+++ app/models/rset_mcm/permission.rb
@@ -1,6 +1,12 @@
 module RsetMcm
   class Permission < ActiveRecord::Base
     belongs_to :content
-    attr_accessible :mode, :resource_id, :resource_type
+    attr_accessible :mode
+
+    belongs_to :resource, :polymorphic => true
+    belongs_to :content
+
+    validates :mode, :presence => true
+    validates :resource_id, :uniqueness => {:scope => [:content_id, :resource_type]}, :presence => true
   end
 end
--- spec/models/rset_mcm/permission_spec.rb
+++ spec/models/rset_mcm/permission_spec.rb
@@ -2,6 +2,22 @@ require 'spec_helper'
 
 module RsetMcm
   describe Permission do
-    pending "add some examples to (or delete) #{__FILE__}"
+    it { should have_db_column(:mode).of_type(:string) }
+    it { should have_db_column(:content_id).of_type(:integer) }
+    it { should have_db_column(:resource_type).of_type(:string) }
+    it { should have_db_column(:resource_id).of_type(:integer) }
+
+    it { should allow_mass_assignment_of(:mode) }
+    it { should_not allow_mass_assignment_of(:content_id) }
+    it { should_not allow_mass_assignment_of(:resource_id) }
+    it { should_not allow_mass_assignment_of(:resource_type) }
+
+    it { should belong_to(:content) }
+    it { should belong_to(:resource) }
+
+    describe 'Validation' do
+      it { should validate_presence_of(:mode) }
+      it { should validate_presence_of(:resource_id) }
+    end
   end
 end
