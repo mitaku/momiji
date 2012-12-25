95565035a9e60eefed78bf121edbe6c2c34e4cb7
Takumi Miura <takumi.miura@optim.co.jp>
2012-12-25 23:02:36 +0900

    rails generate acts_as_taggable_on:migration

--- spec/dummy/db/schema.rb
+++ spec/dummy/db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20121225132012) do
+ActiveRecord::Schema.define(:version => 20121225140158) do
 
   create_table "rset_mcm_contents", :force => true do |t|
     t.string   "name"
@@ -38,5 +38,22 @@ ActiveRecord::Schema.define(:version => 20121225132012) do
 
   add_index "rset_mcm_permissions", ["content_id"], :name => "index_rset_mcm_permissions_on_content_id"
   add_index "rset_mcm_permissions", ["resource_id", "resource_type"], :name => "index_rset_mcm_permissions_on_resource_id_and_resource_type"
+
+  create_table "taggings", :force => true do |t|
+    t.integer  "tag_id"
+    t.integer  "taggable_id"
+    t.string   "taggable_type"
+    t.integer  "tagger_id"
+    t.string   "tagger_type"
+    t.string   "context",       :limit => 128
+    t.datetime "created_at"
+  end
+
+  add_index "taggings", ["tag_id"], :name => "index_taggings_on_tag_id"
+  add_index "taggings", ["taggable_id", "taggable_type", "context"], :name => "index_taggings_on_taggable_id_and_taggable_type_and_context"
+
+  create_table "tags", :force => true do |t|
+    t.string "name"
+  end
 
 end
